ddc3be1769736c6a4ff190fce13e3c0f328a8238
NTUEconSA <111209922+NTUEconSA@users.noreply.github.com>
2023-02-19 09:04:51 +0800

    Add files via upload

---
+++ woman.cpp
@@ -0,0 +1,119 @@
+#include<iostream>
+#include<time.h>
+#include<stdlib.h>
+#include<string>
+using namespace std;
+
+string champ_name[4];
+string second_name[4];
+
+//0123 represent ABCD cycle
+int champ[4] = {0,0,0,0};
+int second[4] = {0,0,0,0};
+
+void random_sort(){
+    srand(time(NULL));
+    int count1 = 0;
+    int used1[4] = {0,0,0,0};
+    while(count1 < 4){
+        int index1 = rand() % 4;
+        if(index1 < 0){
+            index1 = -index1;
+        }
+        if(used1[index1] == 0){
+            used1[index1] = 1;
+            champ[count1] = index1;
+            count1 ++;
+        }
+        else{
+            continue;
+        }
+    }
+
+    int count2 = 0;
+    int used2[4] = {0,0,0,0};
+    while(count2 < 4){
+        int index2 = rand() % 4;
+        if(index2 < 0){
+            index2 = -index2;
+        }
+        if(used2[index2] == 0){
+            used2[index2] = 1;
+            second[count2] = index2;
+            count2 ++;
+        }
+        else{
+            continue;
+        }
+    }
+}
+
+int check(){
+    int again = 0;
+    if(champ[0] == second[0]){
+        again = 1;
+    }
+    if(champ[1] == second[1]){
+        again = 1;
+    }
+    if(champ[2] == second[2]){
+        again = 1;
+    }
+    if(champ[3] == second[3]){
+        again = 1;
+    }
+    if(again == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+//A,B,C 為3取2  //D,E為4取2
+int main(){
+    // cout << "A循環冠軍 : ";
+    cin >> champ_name[0];
+    // cout << "B循環冠軍 : ";
+    cin >> champ_name[1];
+    // cout << "C循環冠軍 : ";
+    cin >> champ_name[2];
+    // cout << "D循環冠軍 : ";
+    cin >> champ_name[3];
+
+    // cout << "A循環亞軍 : ";
+    cin >> second_name[0];
+    // cout << "B循環亞軍 : ";
+    cin >> second_name[1];
+    // cout << "C循環亞軍 : ";
+    cin >> second_name[2];
+    // cout << "D循環亞軍 : ";
+    cin >> second_name[3];
+    
+    random_sort();
+    while(check() == 0){
+        for(int i = 0; i < 4; i++){
+            champ[i] = 0;
+            second[i] = 0;
+        }
+        random_sort();
+    }
+
+    cout << "甲區 : \n";
+    cout << champ_name[champ[0]] << '\n';
+    cout << second_name[second[0]] << '\n';
+
+    cout << "乙區 : \n";
+    cout << champ_name[champ[1]] << '\n'; 
+    cout << second_name[second[1]] << '\n';
+
+    cout << "丙區 : \n";
+    cout << champ_name[champ[2]] << '\n'; 
+    cout << second_name[second[2]] << '\n';
+
+    cout << "丁區 : \n";
+    cout << champ_name[champ[3]] << '\n';
+    cout << second_name[second[3]];
+
+
+}
